ca4a8065c40d9d4d41d7888d52b8a37ad695afdb
uditarora360 <udit.arora@adglobal360.com>
2021-02-05 19:16:26 +0530

    First Commit

---
+++ SSO_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSO_Demo
+//
+//  Created by UDIT ARORA on 05/02/21.
+//  Copyright © 2021 UDIT ARORA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
